3798d1c2f25f8a12f3ee1126d2c1cadbdabfaaa3
z3z1ma <butler.alex2010@gmail.com>
2023-09-23 02:02:12 -0700

    feat: working version of decl environment pattern

--- environments/flake.nix
+++ environments/flake.nix
@@ -3,19 +3,13 @@
   inputs = {
     python.url = "path:./python";
   };
-  outputs = inputs@{ flake-parts, python }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.devenv.flakeModule ];
       flake = { };
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         {
-          devenv.shells.python310 = python.devShells."${system}".python310;
+          devShells = { } // inputs.python.outputs.devShells."${system}";
         };
     };
 }
