328cc3232ebcefb8671aa416b1966b5201e33178
z3z1ma <butler.alex2010@gmail.com>
2023-10-05 15:00:05 +0100

    fix: drop explicit config for indentline v3

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -192,24 +192,7 @@ local M = {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
+    config = false
   },
 
   -- active indent guide and indent text objects. When you're browsing
