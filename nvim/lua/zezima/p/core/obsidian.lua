7d090c67af2147488ac4e286ea42fd3227660af0
z3z1ma <butler.alex2010@gmail.com>
2023-10-22 14:32:10 -0700

    feat: improve obsidian support

--- nvim/lua/zezima/p/core/obsidian.lua
+++ nvim/lua/zezima/p/core/obsidian.lua
@@ -3,9 +3,10 @@
 return {
   "epwalsh/obsidian.nvim",
   lazy = true,
+  cmd = { "ObsidianToday", "ObsidianYesterday", "ObsidianSearch", "ObsidianWorkspace" },
   event = {
-    "BufReadPre " .. vim.fn.expand "~" .. "vaults/**.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "vaults/**.md",
+    "BufReadPre " .. vim.fn.expand "~" .. "/vaults/**.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/vaults/**.md",
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -13,6 +14,68 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   opts = {
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+      new_notes_location = "current_dir",
+      prepend_note_id = true
+    },
+    notes_subdir = "notes",
+    daily_notes = {
+      folder = "notes/dailies",
+      date_format = "%Y-%m-%d",
+      alias_format = "%B %-d, %Y",
+      template = nil
+    },
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {}
+    },
+    open_notes_in = "current", -- "current", "hsplit" or "vsplit"
+    yaml_parser = "native",    -- "native" or "yq"
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end,
+    note_frontmatter_func = function(note)
+      -- This is equivalent to the default frontmatter function.
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+    },
+    overwrite_mappings = true,
+    sort_by = "modified",
+    sort_reversed = true,
     workspaces = {
       {
         name = "personal",
