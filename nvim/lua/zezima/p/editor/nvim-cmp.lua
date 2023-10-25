14577b1a10a8ea35e2c33c8c631e05da5c8aeced
z3z1ma <butler.alex2010@gmail.com>
2023-10-24 21:50:13 -0700

    feat: dont expand completions with tab unless explicit or shift tabbed
    ergonomics may take a few days but it is immensely better than C-e to close the menu which is constantly popping up just to be able to insert a line break or tab

--- nvim/lua/zezima/p/editor/nvim-cmp.lua
+++ nvim/lua/zezima/p/editor/nvim-cmp.lua
@@ -15,38 +15,50 @@ return {
     },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
+      local cmp = require "cmp"
+      local defaults = require "cmp.config.default"()
       table.insert(defaults.sorting.comparators, 1, require("copilot_cmp.comparators").prioritize)
       return {
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = "menu,menuone,noinsert,noselect,preview",
         },
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        preselect = cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert {
+          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
-        sources = cmp.config.sources({
-          { name = "copilot",  group_index = 2 },
+          ["<Tab>"] = cmp.mapping {
+            i = cmp.mapping.confirm { select = false },
+            s = cmp.mapping.confirm { select = true },
+            c = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
+          },
+          ["<S-Tab>"] = cmp.mapping(cmp.mapping(function(fallback)
+            if cmp.visible() then
+              local entry = cmp.get_selected_entry()
+              if not entry then
+                cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+              end
+              cmp.confirm()
+            else
+              fallback()
+            end
+          end, { "i", "s", "c" })),
+        }, -- this simplifies quick completions while preserving normal editor behavior for enter/tab in insert mode
+        sources = cmp.config.sources {
+          { name = "copilot", group_index = 2 },
           { name = "nvim_lsp", group_index = 2 },
-          { name = "buffer",   group_index = 2 },
-          { name = "path",     group_index = 2 },
-          { name = "luasnip",  group_index = 2 },
-        }),
+          { name = "buffer", group_index = 2 },
+          { name = "path", group_index = 2 },
+          { name = "luasnip", group_index = 2 },
+        },
         formatting = {
           format = function(_, item)
             local icons = require("zezima.constants").icons.kinds
@@ -64,5 +76,5 @@ return {
         sorting = defaults.sorting,
       }
     end,
-  }
+  },
 }
