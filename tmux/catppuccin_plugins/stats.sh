4b75ea367d671e6befb7f29c0c9dda6b45594715
z3z1ma <butler.alex2010@gmail.com>
2023-10-29 21:18:07 -0700

    feat: add next meeting to statusline

--- tmux/catppuccin_plugins/stats.sh
+++ tmux/catppuccin_plugins/stats.sh
@@ -3,7 +3,7 @@ show_stats() {
     "$1"\
     "#{battery_icon_charge}"\
     "$thm_yellow"\
-    "#[fg=$thm_yellow]BAT: #{battery_percentage} #{battery_remain} | #[fg=$thm_magenta]CPU: #{cpu_icon} #{cpu_percentage} | #[fg=$thm_cyan]RAM: #{ram_icon} #{ram_percentage}"\
+    "#[fg=$thm_yellow]BAT: #{battery_percentage} #{battery_remain} | #[fg=$thm_magenta]CPU: #{cpu_icon} #{cpu_percentage} |#[fg=$thm_cyan]#(head -2 /tmp/gcalcli.txt | cut -d ' ' -f 4- | tail -1 | tr -s '[:space:]')"\
   )
   echo "$module"
 }
