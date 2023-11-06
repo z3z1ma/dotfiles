a35259b4ccaae96efac1d2d23a512a3fe1df6b76
z3z1ma <butler.alex2010@gmail.com>
2023-11-06 10:02:08 -0700

    feat: add symbol outline viewer, very sweet

---
+++ nvim/lua/zezima/p/editor/symbols-outline.lua
@@ -0,0 +1,8 @@
+-- Repo: https://github.com/simrat39/symbols-outline.nvim
+-- Description: A tree like view for symbols in Neovim using the Language Server Protocol. Supports all your favourite languages.
+return {
+  {
+    "simrat39/symbols-outline.nvim",
+    opts = {},
+  },
+}
