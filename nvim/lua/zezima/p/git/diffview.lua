c523e0830202fb3e6d07f6c1195692380ef63765
z3z1ma <butler.alex2010@gmail.com>
2023-11-07 17:31:39 -0700

    feat: more highlighting in diffview

--- nvim/lua/zezima/p/git/diffview.lua
+++ nvim/lua/zezima/p/git/diffview.lua
@@ -10,19 +10,35 @@ return {
     lazy = true,
     config = function()
       vim.opt.fillchars = "diff:░"
-      require("diffview").setup({
+      require("diffview").setup {
         enhanced_diff_hl = true,
-      })
+        hooks = {
+          diff_buf_win_enter = function(bufnr, winid, ctx)
+            if ctx.layout_name:match "^diff2" then
+              if ctx.symbol == "a" then
+                vim.opt_local.winhl = table.concat({
+                  "DiffAdd:DiffviewDiffAddAsDelete",
+                  "DiffDelete:DiffviewDiffDelete",
+                }, ",")
+              elseif ctx.symbol == "b" then
+                vim.opt_local.winhl = table.concat({
+                  "DiffDelete:DiffviewDiffDelete",
+                }, ",")
+              end
+            end
+          end,
+        },
+      }
     end,
     keys = {
       -- use [c and [c to navigate diffs (vim built in), see :h jumpto-diffs
       -- use ]x and [x to navigate conflicts
-      { "<leader>gdc", ":DiffviewOpen origin/main...HEAD",               desc = "Compare commits" },
-      { "<leader>gdd", ":DiffviewClose<CR>",                             desc = "Close Diffview tab" },
-      { "<leader>gdh", ":DiffviewFileHistory %<CR>",                     desc = "File history" },
-      { "<leader>gdH", ":DiffviewFileHistory<CR>",                       desc = "Repo history" },
-      { "<leader>gdm", ":DiffviewOpen<CR>",                              desc = "Solve merge conflicts" },
-      { "<leader>gdo", ":DiffviewOpen main",                             desc = "DiffviewOpen" },
+      { "<leader>gdc", ":DiffviewOpen origin/main...HEAD", desc = "Compare commits" },
+      { "<leader>gdd", ":DiffviewClose<CR>", desc = "Close Diffview tab" },
+      { "<leader>gdh", ":DiffviewFileHistory %<CR>", desc = "File history" },
+      { "<leader>gdH", ":DiffviewFileHistory<CR>", desc = "Repo history" },
+      { "<leader>gdm", ":DiffviewOpen<CR>", desc = "Solve merge conflicts" },
+      { "<leader>gdo", ":DiffviewOpen main", desc = "DiffviewOpen" },
       { "<leader>gdp", ":DiffviewOpen origin/main...HEAD --imply-local", desc = "Review current PR" },
       {
         "<leader>gdP",
@@ -30,5 +46,5 @@ return {
         desc = "Review current PR (per commit)",
       },
     },
-  }
+  },
 }
