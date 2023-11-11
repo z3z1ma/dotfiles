d98a334bcb69cbcc0ea14b1d046c6b147381a51d
z3z1ma <butler.alex2010@gmail.com>
2023-11-11 13:39:04 -0700

    feat: use new updated symbols outline fork

--- nvim/lua/zezima/p/editor/symbols-outline.lua
+++ nvim/lua/zezima/p/editor/symbols-outline.lua
@@ -2,7 +2,7 @@
 -- Description: A tree like view for symbols in Neovim using the Language Server Protocol. Supports all your favourite languages.
 return {
   {
-    "simrat39/symbols-outline.nvim",
+    "hedyhli/symbols-outline.nvim",
     opts = {},
   },
 }
