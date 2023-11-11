b64b154b5fc2bbded83c1badd6d6e2a6c161c652
z3z1ma <butler.alex2010@gmail.com>
2023-11-10 20:03:32 -0700

    chore: shorten tmux status bar for now

--- tmux/catppuccin_plugins/stats.sh
+++ tmux/catppuccin_plugins/stats.sh
@@ -3,7 +3,8 @@ show_stats() {
     "$1"\
     "#{battery_icon_charge}"\
     "$thm_yellow"\
-    "#[fg=$thm_yellow]BAT: #{battery_percentage} #{battery_remain} | #[fg=$thm_magenta]CPU: #{cpu_icon} #{cpu_percentage} |#[fg=$thm_cyan]#(head -2 /tmp/gcalcli.txt | cut -d ' ' -f 4- | tail -1 | tr -s '[:space:]')"\
+    "#[fg=$thm_yellow]BAT: #{battery_percentage} #{battery_remain} | #[fg=$thm_magenta]CPU: #{cpu_icon} #{cpu_percentage}"\
   )
   echo "$module"
 }
+# |#[fg=$thm_cyan]#(head -2 /tmp/gcalcli.txt | cut -d ' ' -f 4- | tail -1 | tr -s '[:space:]'
