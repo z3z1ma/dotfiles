ae70e4e9ae1361b125a291bacfcdeeb3d5e86114
z3z1ma <butler.alex2010@gmail.com>
2023-11-15 13:36:09 -0700

    fix: update for new inlay hint api in neovim nightly

--- nvim/lua/zezima/p/editor/nvim-lspconfig.lua
+++ nvim/lua/zezima/p/editor/nvim-lspconfig.lua
@@ -172,7 +172,7 @@ return {
       end
 
       -- Inlay hints if enabled
-      local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+      local inlay_hint = vim.lsp.inlay_hint.enable
       if opts.inlay_hints.enabled and inlay_hint then
         Z.lsp.on_attach(function(client, buffer)
           if client.supports_method("textDocument/inlayHint") then
