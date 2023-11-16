93f508739df03e45bb3e3f427d77a850bfa27791
z3z1ma <butler.alex2010@gmail.com>
2023-11-15 17:50:17 -0700

    feat: add smart column

---
+++ nvim/lua/zezima/p/editor/smartcolumn.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = "120",
+      disabled_filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+}
