3962c0ae5c03ebffdf10f94a8c2b4425acb32a57
z3z1ma <butler.alex2010@gmail.com>
2023-11-15 18:43:18 -0700

    feat: add tpope rhubarb plug gor GBrowse

--- nvim/lua/zezima/p/git/vim-fugitive.lua
+++ nvim/lua/zezima/p/git/vim-fugitive.lua
@@ -1,5 +1,8 @@
 -- Repo: https://github.com/tpope/vim-fugitive
 -- Description: fugitive.vim: A Git wrapper so awesome, it should be illegal
 return {
-  { "tpope/vim-fugitive" },
+  {
+    "tpope/vim-fugitive",
+    dependencies = { "tpope/vim-rhubarb" },
+  },
 }
