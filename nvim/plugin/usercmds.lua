4026c60d80fff534b9dfcf6f58406e83b0b20f69
z3z1ma <butler.alex2010@gmail.com>
2023-11-17 23:17:01 -0700

    feat: add messages user command

--- nvim/plugin/usercmds.lua
+++ nvim/plugin/usercmds.lua
@@ -1,6 +1,19 @@
+-- Toggle transparency in catppuccin
 vim.api.nvim_create_user_command("CatppuccinTransparency", function()
   local cat = require("catppuccin")
   cat.options.transparent_background = not cat.options.transparent_background
   cat.compile()
   vim.cmd.colorscheme(vim.g.colors_name)
 end, { desc = "Toggle transparency" })
+
+-- Open messages in a split, these include command output
+vim.api.nvim_create_user_command("Messages", function()
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_call(bufnr, function()
+    vim.cmd([[put= execute('messages')]])
+  end)
+  vim.api.nvim_set_option_value("modifiable", false, { buf = bufnr })
+  vim.cmd.split()
+  local winnr = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(winnr, bufnr)
+end, {})
