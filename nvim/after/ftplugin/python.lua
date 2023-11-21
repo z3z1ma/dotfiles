3e7c04b753aa6427b61a63c1293fb39b901d377f
z3z1ma <butler.alex2010@gmail.com>
2023-11-20 19:55:01 -0700

    fix: comment py kernel user cmd for now

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,20 +1,22 @@
+-- TODO: explore why this doesn't work
+
 -- Small convenience to make a new kernel for the current virtualenv
-vim.api.nvim_create_user_command("PythonMakeKernel", function(opts)
-  local env_name = opts.args[1]
-  if env_name == nil then
-    print("Please specify environment name")
-    return
-  end
-  vim.system({ "python", "-m", "pip", "install", "ipykernel" })
-  vim.system({
-    "python",
-    "-m",
-    "ipykernel",
-    "install",
-    "--user",
-    "--name",
-    env_name,
-    "--display-name",
-    "Python (" .. env_name .. ")",
-  })
-end)
+-- vim.api.nvim_create_user_command("PythonMakeKernel", function()
+--   local env_name = opts.args[1]
+--   if env_name == nil then
+--     print("Please specify environment name")
+--     return
+--   end
+--   vim.system({ "python", "-m", "pip", "install", "ipykernel" })
+--   vim.system({
+--     "python",
+--     "-m",
+--     "ipykernel",
+--     "install",
+--     "--user",
+--     "--name",
+--     env_name,
+--     "--display-name",
+--     "Python (" .. env_name .. ")",
+--   })
+-- end)
