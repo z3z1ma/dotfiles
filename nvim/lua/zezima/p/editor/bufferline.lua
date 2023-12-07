9468402b61bb8db3309c7ff02769d7bf66ecda1d
z3z1ma <butler.alex2010@gmail.com>
2023-12-07 16:29:48 -0700

    feat: bufferline improvements and nice keymap to jump bufs

--- nvim/lua/zezima/p/editor/bufferline.lua
+++ nvim/lua/zezima/p/editor/bufferline.lua
@@ -5,11 +5,26 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     keys = {
+      { "<C-1>", "<Cmd>BufferLineGoToBuffer 1<CR>", desc = "Go to buffer 1" },
+      { "<C-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", desc = "Go to buffer 2" },
+      { "<C-3>", "<Cmd>BufferLineGoToBuffer 3<CR>", desc = "Go to buffer 3" },
+      { "<C-4>", "<Cmd>BufferLineGoToBuffer 4<CR>", desc = "Go to buffer 4" },
+      { "<C-5>", "<Cmd>BufferLineGoToBuffer 5<CR>", desc = "Go to buffer 5" },
+      { "<C-6>", "<Cmd>BufferLineGoToBuffer 6<CR>", desc = "Go to buffer 6" },
+      { "<C-7>", "<Cmd>BufferLineGoToBuffer 7<CR>", desc = "Go to buffer 7" },
+      { "<C-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", desc = "Go to buffer 8" },
+      { "<C-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", desc = "Go to buffer 9" },
+      { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
+      { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer" },
+      { "<A-S-l>", "<Cmd>BufferLineMoveNext<CR>", desc = "Move buffer right" },
+      { "<A-S-h>", "<Cmd>BufferLineMovePrev<CR>", desc = "Move buffer left" },
       { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
       { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
     },
     opts = {
       options = {
+        separator_style = { "", "" },
+        indicator = { style = "underline" },
         close_command = function(n)
           require("mini.bufremove").delete(n, false)
         end,
@@ -24,12 +39,18 @@ return {
             .. (diag.warning and icons.Warn .. diag.warning or "")
           return vim.trim(ret)
         end,
+        hover = {
+          enabled = true,
+          delay = 0,
+          reveal = { "close" },
+        },
         offsets = {
           {
             filetype = "neo-tree",
-            text = "Neo-tree",
+            text = "EXPLORER",
             highlight = "Directory",
-            text_align = "left",
+            text_align = "center",
+            separator = true,
           },
         },
       },
