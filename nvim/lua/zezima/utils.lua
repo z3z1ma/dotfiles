2d528edef1c98284be06df27d95ba9c1d1d0093a
z3z1ma <butler.alex2010@gmail.com>
2023-12-07 16:28:05 -0700

    feat: add borger gen util function

--- nvim/lua/zezima/utils.lua
+++ nvim/lua/zezima/utils.lua
@@ -404,4 +404,66 @@ function M.get_upvalue(func, name)
   end
 end
 
+--- @param type "thin" | "thick" | "empty" | nil
+--- @param order "t-r-b-l-tl-tr-br-bl" | "tl-t-tr-r-bl-b-br-l" | nil
+--- @param opts BorderIcons | nil
+M.generate_borderchars = function(type, order, opts)
+  if order == nil then
+    order = "t-r-b-l-tl-tr-br-bl"
+  end
+  local border_icons = require("zezima.constants").icons.borders
+  --- @type BorderIcons
+  local border = vim.tbl_deep_extend("force", border_icons[type or "empty"], opts or {})
+
+  local borderchars = {}
+
+  local extractDirections = (function()
+    local index = 1
+    return function()
+      if index == nil then
+        return nil
+      end
+      -- Find the next occurence of `char`
+      local nextIndex = string.find(order, "-", index)
+      -- Extract the first direction
+      local direction = string.sub(order, index, nextIndex and nextIndex - 1)
+      -- Update the index to nextIndex
+      index = nextIndex and nextIndex + 1 or nil
+      return direction
+    end
+  end)()
+
+  local mappings = {
+    t = "top",
+    r = "right",
+    b = "bottom",
+    l = "left",
+    tl = "top_left",
+    tr = "top_right",
+    br = "bottom_right",
+    bl = "bottom_left",
+  }
+  local direction = extractDirections()
+  while direction do
+    if mappings[direction] == nil then
+      M.notify(string.format("Invalid direction '%s'", direction), "error")
+    end
+    borderchars[#borderchars + 1] = border[mappings[direction]]
+    direction = extractDirections()
+  end
+
+  if #borderchars ~= 8 then
+    M.notify(string.format("Invalid order '%s'", order), "error")
+  end
+
+  return borderchars
+end
+
+-- Generates an augroup name prefixed with zezima_ to avoid collisions
+---@param name string
+---@return number
+function M.augroup(name)
+  return vim.api.nvim_create_augroup("zezima_" .. name, { clear = true })
+end
+
 return M
