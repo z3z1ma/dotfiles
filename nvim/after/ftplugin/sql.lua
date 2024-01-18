47a6acf4f1c837cd47a12c423fae435ed89c0981
z3z1ma <butler.alex2010@gmail.com>
2024-01-17 19:19:32 -0700

    chore(nvim): simplify sql ftplugin

--- nvim/after/ftplugin/sql.lua
+++ nvim/after/ftplugin/sql.lua
@@ -1,62 +1,36 @@
-local function extract_model_name()
-  -- Read the entire buffer content
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  local content = table.concat(lines, "\n")
-
-  -- Extract the model name using Lua pattern matching
-  local model_name = content:match "MODEL %(%s*name%s+([^,]+),"
-
-  -- Store the extracted string in a table
-  local some = {}
-  some.table = model_name
-
-  return some
-end
-
-local function get_project_root()
-  -- Search for config.py / config.yaml / config.yml going up the directory tree from filepath
-  local config_path = vim.fn.findfile("config.py", vim.fn.expand "%:p:h" .. ";")
-  if config_path == "" then
-    config_path = vim.fn.findfile("config.yaml", vim.fn.expand "%:p:h" .. ";")
-  end
-  if config_path == "" then
-    config_path = vim.fn.findfile("config.yml", vim.fn.expand "%:p:h" .. ";")
-  end
-  local some = {}
-  some.table = config_path and vim.fn.fnamemodify(config_path, ":h") or nil
-  return some
-end
+vim.opt_local.foldmethod = "indent"
 
 vim.keymap.set("n", "<leader>C", function()
-  local Job = require "plenary.job"
+  local Job = require("plenary.job")
 
   -- Get the project root path
-  local project_root = get_project_root().table
+  local root_marker = vim.fn.findfile("cdf_config.toml", vim.fn.expand("%:p:h") .. ";")
+  local project_root = root_marker and vim.fn.fnamemodify(root_marker, ":h") or nil
   if project_root == nil then
-    vim.notify "Could not find config.py / config.yaml / config.yml in the current directory or any parent directory"
+    vim.notify("Could not find cdf_config.toml in the current directory or any parent directory")
     return
   end
 
   -- Get the model name from the current buffer
-  local model_name = extract_model_name().table
+  local model_name = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), "\n"):match("MODEL %(%s*name%s+([^,]+),")
   if model_name == nil then
-    vim.notify "Could not find model name in the current buffer"
+    vim.notify("Could not find model name in the current buffer")
     return
   end
 
   -- Run the SQLMesh command
   Job:new({
-    command = "sqlmesh",
-    args = { "-p", project_root, "render", "--no-format", model_name },
+    command = "cdf",
+    args = { "transform", "bigquery", "render", "--no-format", model_name },
     on_start = function()
       vim.notify("Compiling model " .. model_name .. " in " .. project_root)
     end,
     on_exit = function(j, code)
       if code == 0 then
-        vim.notify "SQLMesh render successful"
+        vim.notify("SQLMesh render successful")
         vim.schedule(function()
           -- Create a new buffer
-          vim.cmd "vnew"
+          vim.cmd("vnew")
           -- Ensure buffer is temporary
           vim.bo.buftype = "nofile"
           -- Set ft to SQL
