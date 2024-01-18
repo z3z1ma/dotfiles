ea61a20b7d138a4132ff8a5507873ad58b2c2d59
z3z1ma <butler.alex2010@gmail.com>
2024-01-17 19:19:54 -0700

    chore(nvim): comment out default luasnip snips

--- nvim/lua/zezima/p/hacking/luasnip.lua
+++ nvim/lua/zezima/p/hacking/luasnip.lua
@@ -6,7 +6,7 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+        -- require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_snipmate").lazy_load()
       end,
     },
