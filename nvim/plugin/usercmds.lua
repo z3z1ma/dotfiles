88c386674320eb1b37bd4f009901e3e2cd4feae6
z3z1ma <butler.alex2010@gmail.com>
2024-01-17 19:20:31 -0700

    feat(nvim): convenient user command to edit or create snippets

--- nvim/plugin/usercmds.lua
+++ nvim/plugin/usercmds.lua
@@ -18,3 +18,24 @@ vim.api.nvim_create_user_command("Messages", function()
   local winnr = vim.api.nvim_get_current_win()
   vim.api.nvim_win_set_buf(winnr, bufnr)
 end, {})
+
+-- Edit snippets
+vim.api.nvim_create_user_command("LuaSnipEdit", function()
+  require("luasnip.loaders").edit_snippet_files({
+    extend = function(ft, paths)
+      if ft == "" then
+        ft = "all"
+      end
+      if #paths == 0 then
+        return {
+          {
+            vim.fn.stdpath("config") .. "/snippets/" .. ft .. ".snippets",
+            vim.fn.stdpath("config") .. "/snippets/" .. ft .. ".lua",
+          },
+        }
+      end
+
+      return {}
+    end,
+  })
+end, {})
