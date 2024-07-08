7d03f91cc42754f9408dd00e92a0373e2993ece8
z3z1ma <butler.alex2010@gmail.com>
2024-07-07 23:22:19 -0700

    chore: bump plugins and add harness tui keybind

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -128,7 +128,7 @@ vim.keymap.set("n", "<leader>gG", function()
   Z.vim.float_term({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
 end, { desc = "Lazygit (cwd)" })
 vim.keymap.set("n", "<leader>gH", function()
-  Z.vim.float_term({ "textual", "run", "--dev", "src/harness_tui/app.py" }, { esc_esc = false, ctrl_hjkl = false })
+  Z.vim.float_term({ "harness-tui" }, { esc_esc = false, ctrl_hjkl = false })
 end, { desc = "Lazygit (cwd)" })
 
 -- Quit
