d84e44b3712f9c2b0207f1b5d66448dcc7c65f10
z3z1ma <butler.alex2010@gmail.com>
2024-07-31 18:11:31 -0700

    feat: add sendmail conf to git, update conform fmt opts

--- nvim/lua/zezima/p/editor/conform.lua
+++ nvim/lua/zezima/p/editor/conform.lua
@@ -6,18 +6,19 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { { "ruff_organize_imports", "ruff_format" }, { "black", "isort" } },
-        javascript = { { "prettierd", "prettier" } },
+        python = { "black", "isort" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
         go = { "gofmt", "goimports" },
-        typescript = { { "prettierd", "prettier" } },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
         rust = { "rustfmt" },
         nix = { "nixfmt" },
         scheme = { "racofmt" },
+        ["_"] = { "trim_whitespace" },
       },
-      format_on_save = {
-        timeout_ms = 5000,
-        lsp_fallback = true,
-      },
+      -- format_on_save = {
+      --   timeout_ms = 5000,
+      --   lsp_fallback = true,
+      -- },
       formatters = {
         injected = { options = { ignore_errors = true } },
         sqlfmt = {
@@ -29,9 +30,30 @@ return {
           args = { "fmt", "--width", 50 },
         },
       },
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
+      },
     },
     init = function()
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      -- vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      require("zezima.format").register({
+        name = "conform.nvim",
+        priority = 100,
+        primary = true,
+        format = function(buf)
+          require("conform").format({ bufnr = buf })
+        end,
+        sources = function(buf)
+          local ret = require("conform").list_formatters(buf)
+          ---@param v conform.FormatterInfo
+          return vim.tbl_map(function(v)
+            return v.name
+          end, ret)
+        end,
+      })
     end,
   },
 }
