fa06dcd9f8ab7b925caa2d5ac05be0345d2d2a45
z3z1ma <butler.alex2010@gmail.com>
2024-11-14 21:26:42 -0700

    feat(nvim): use blink instead of nvim cmp

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.coding.neogen" },
+  { import = "lazyvim.plugins.extras.coding.blink" },
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
   { import = "lazyvim.plugins.extras.editor.telescope" },
   { import = "lazyvim.plugins.extras.editor.mini-diff" },
