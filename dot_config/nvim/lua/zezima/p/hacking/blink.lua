c8a2f010fe5b66e825b796cef860be93af9f965c
z3z1ma <butler.alex2010@gmail.com>
2024-12-02 12:21:37 -0700

    chore(nvim): bump all plugin pins, have blink skeleton config to disable/reenable for upgrade

--- dot_config/nvim/lua/zezima/p/hacking/blink.lua
+++ dot_config/nvim/lua/zezima/p/hacking/blink.lua
@@ -1,15 +1,4 @@
 return {
-  "saghen/blink.cmp",
-  version = "0.5.1",
-  dependencies = { "kristijanhusak/vim-dadbod-completion" },
-  opts = {
-    sources = {
-      completion = {
-        enabled_providers = { "lsp", "path", "snippets", "buffer", "dadbod" },
-      },
-      providers = {
-        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-      },
-    },
-  },
+  "Saghen/blink.cmp",
+  enabled = true,
 }
