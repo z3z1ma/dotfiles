8be872ca0802cf1346b700834a430437ca3c27a3
z3z1ma <butler.alex2010@gmail.com>
2025-05-29 22:00:38 -0700

    feat(nvim): add start of sqlmesh lsp code

--- dot_config/nvim/after/ftplugin/sql.lua
+++ dot_config/nvim/after/ftplugin/sql.lua
@@ -1,10 +1,30 @@
 vim.opt_local.foldmethod = "indent"
 
--- if vim.fn.filereadable("sql_lsp.py") == 1 then
---   vim.lsp.start({
---     name = "sqlmesh-lsp",
---     cmd = { "python", "sql_lsp.py" },
---     root_dir = vim.fs.root(0, { "pyproject.toml", "setup.py", "requirements.txt" }),
---     reuse_client = true,
---   })
--- end
+vim.lsp.config["sqlmesh_lsp"] = {
+  cmd = { "sqlmesh_lsp" },
+  filetypes = { "sql" },
+  root_markers = { { "tcloud.yaml", "tcloud.yml" }, { "config.py", "config.yaml", "config.yml" } },
+  settings = {
+    -- NOTE: does this work?
+    -- projectPath = "workspaces/datateam",
+  },
+}
+
+vim.lsp.enable("sqlmesh_lsp")
+
+vim.api.nvim_create_user_command("StartSQLMeshLSP", function(opts)
+  local root = opts.args ~= "" and vim.fn.expand(opts.args)
+    or vim.fs.root(0, { "config.py", "tcloud.yml", "tcloud.yaml", "config.yml", "config.yaml" })
+
+  vim.lsp.start({
+    name = "sqlmesh-lsp",
+    cmd = { "sqlmesh_lsp" },
+    root_dir = root,
+    reuse_client = true,
+    cmd_env = { SQLMESH_IGNORE_WARNINGS = "1" },
+  })
+end, {
+  nargs = "?",
+  complete = "dir",
+  desc = "Start sqlmesh-lsp with optional root path",
+})
