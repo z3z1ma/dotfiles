da1936dfa6f73306c5c8d05634befd4b6f75e32b
z3z1ma <butler.alex2010@gmail.com>
2025-09-14 13:00:18 -0700

    feat(fish): sandbox build abbr for harness

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -23,3 +23,5 @@ abbr updnix "nix-channel --update"
 abbr updnixdarwin "darwin-rebuild switch --flake ~/.config/nix_system#$(scutil --get LocalHostName)"
 
 abbr cd z
+
+abbr harnessb SDKROOT=(xcrun --sdk macosx --show-sdk-path) DEVELOPER_DIR=(xcode-select -p) bazel build --repo_env=CC=/usr/bin/clang --repo_env=CXX=/usr/bin/clang++ --action_env=DEVELOPER_DIR --action_env=SDKROOT --action_env=PATH=/usr/bin:/bin:/usr/sbin:/sbin
